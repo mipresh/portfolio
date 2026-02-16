6956a8d60209ed3473b1f5aa35d9bc9ce5ef52ee
Presh Mi <mmesoprecious9@gmaill.com>
2026-02-16 15:25:03 -0800

    update

--- script.js
+++ script.js
@@ -32,6 +32,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
  
+const burger = document.getElementById("hamburger");
+const nav = document.getElementById("navLinks");
+
+burger.addEventListener("click", ()=>{
+  nav.classList.toggle("active");
+});
+
+
+ 
   const hamburger = document.getElementById("hamburger");
   const navLinks = document.querySelector(".nav-links");
 
